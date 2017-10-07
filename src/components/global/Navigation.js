98b2576c5d5b6cbeaeb946c460e8096f02848e36
trevans24 <trevans24@gmail.com>
2017-10-07 05:43:36 -0600

    made a toggle menu with drawer

--- src/components/global/Navigation.js
+++ src/components/global/Navigation.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import '../../css/components/nav.css';
 
 export default class Nav extends Component {
+	constructor() {
+	  super();
+	
+	  this.state = {
+	  	open: false,
+	  };
+	}
+
+	_toggle = (e) => {
+		e.preventDefault();
+		this.setState({
+			open: !this.state.open
+		});
+	}
+
 	render() {
 		return(
-			<AppBar
-				title="Bellevue Farmers Market"
-				className="app-bar"
-			/>
+			<div>
+				<AppBar
+					title="Bellevue Farmers Market"
+					className="app-bar"
+					onLeftIconButtonTouchTap={this._toggle}
+				/>
+				<Drawer
+					docked={false}
+					open={this.state.open}
+					onRequestChange={open => this.setState({open})}
+				>
+					<MenuItem>The Market</MenuItem>
+					<MenuItem>Vendors</MenuItem>
+					<MenuItem>Get Involved</MenuItem>
+					<MenuItem>Resources</MenuItem>
+					<MenuItem>Social</MenuItem>
+					<MenuItem>Donate</MenuItem>
+					<MenuItem>Find Us</MenuItem>
+					<MenuItem>About Us</MenuItem>
+				</Drawer>
+			</div>
 		)
 	}
 }
