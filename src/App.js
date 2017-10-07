5cfb5dcea0521feac9cb7fba7e6bc56196ebe159
trevans24 <trevans24@gmail.com>
2017-10-07 05:21:33 -0600

    added material ui, added app bar to nav, changed material theme colors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+// components
+import Nav from './components/global/Navigation';
+
+// Styles
+require('./css/Global.css');
+// THEME for Material UI
+const theme = getMuiTheme({
+  palette: {
+    primary1Color: '#8BC34A',
+    primary2Color: '#DCEDC8',
+    primary3Color: '#689F38',
+    accent1Color: '#FF4081',
+    textColor: '#212121',
+    alternateTextColor: '#757575',
+    shadowColor: '#BDBDBD'
+  },
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>fuck you react</h1>
+        <MuiThemeProvider muiTheme={theme} >
+          <Nav />
+          <h1>fuck you react</h1>
+        </MuiThemeProvider>
       </div>
     );
   }
--- src/components/global/Navigation.js
+++ src/components/global/Navigation.js
@@ -1,1 +1,14 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import '../../css/components/nav.css';
+
+export default class Nav extends Component {
+	render() {
+		return(
+			<AppBar
+				title="Bellevue Farmers Market"
+				className="app-bar"
+			/>
+		)
+	}
+}
